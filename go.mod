f7d4b9a0d0ce76988f9f32f75e60d478994af1eb
takenakasuji <t.nksj.e.89@gmail.com>
2020-05-17 10:27:02 +0900

    add jsonpointer pattern

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module json_example
 
 go 1.13
 
-require github.com/koron/go-dproxy v1.3.0
+require (
+	github.com/koron/go-dproxy v1.3.0
+	github.com/mattn/go-jsonpointer v0.0.1
+)
--- json_example_test.go
+++ json_example_test.go
@@ -4,12 +4,13 @@ import (
 	"testing"
 
 	"github.com/koron/go-dproxy"
+	"github.com/mattn/go-jsonpointer"
 )
 
 func TestUnmarshalByInterface(t *testing.T) {
 	var jsonBlob = []byte(`[
 		{"ProductName": "Mac", "Amount": 1, "Info": {"Category": "Laptop", "Tags": ["New", "13inch"]}},
-		{"ProductName": "iPhone",    "Amount": 2, "Info": {"Category": "SmartPhone", "Tags": ["6inch", "New"]}}
+		{"ProductName": "iPhone", "Amount": 2, "Info": {"Category": "SmartPhone", "Tags": ["6inch", "New"]}}
 	]`)
 	o, err := Unmarshal(jsonBlob)
 	if err != nil {
@@ -36,7 +37,7 @@ func TestUnmarshalByInterface(t *testing.T) {
 func TestUnmarshalByDproxy(t *testing.T) {
 	var jsonBlob = []byte(`[
 		{"ProductName": "Mac", "Amount": 1, "Info": {"Category": "Laptop", "Tags": ["New", "13inch"]}},
-		{"ProductName": "iPhone",    "Amount": 2, "Info": {"Category": "SmartPhone", "Tags": ["6inch", "New"]}}
+		{"ProductName": "iPhone", "Amount": 2, "Info": {"Category": "SmartPhone", "Tags": ["6inch", "New"]}}
 	]`)
 	o, err := Unmarshal(jsonBlob)
 	if err != nil {
@@ -57,5 +58,32 @@ func TestUnmarshalByDproxy(t *testing.T) {
 	tg, _ := dproxy.New(o).A(0).M("Info").M("Tags").A(0).String()
 	if tg != "New" {
 		t.Error("TestUnmarshalByDproxy(): Tags failed")
+	}
+}
+
+func TestUnmarshalByJsonPointer(t *testing.T) {
+	var jsonBlob = []byte(`[
+		{"ProductName": "Mac", "Amount": 1, "Info": {"Category": "Laptop", "Tags": ["New", "13inch"]}},
+		{"ProductName": "iPhone", "Amount": 2, "Info": {"Category": "SmartPhone", "Tags": ["6inch", "New"]}}
+	]`)
+	o, err := Unmarshal(jsonBlob)
+	if err != nil {
+		t.Error("TestUnmarshalByJsonPointer(): unmarshal failed")
+	}
+	p, _ := jsonpointer.Get(o, "/0/ProductName")
+	if p != "Mac" {
+		t.Error("TestUnmarshalByJsonPointer(): ProductName failed")
+	}
+	a, _ := jsonpointer.Get(o, "/1/Amount")
+	if a.(float64) != 2 {
+		t.Error("TestUnmarshalByJsonPointer(): Amount failed")
+	}
+	c, _ := jsonpointer.Get(o, "/0/Info/Category")
+	if c != "Laptop" {
+		t.Error("TestUnmarshalByJsonPointer(): Category failed")
+	}
+	tg, _ := jsonpointer.Get(o, "/0/Info/Tags/0")
+	if tg != "New" {
+		t.Error("TestUnmarshalByJsonPointer(): Tags failed")
 	}
 }
